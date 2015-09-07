ffb521fbd42b8bcb65aa09f459083ebec7ef5c8c
1stbit-ito <rails@ip-172-31-18-163.ap-northeast-1.compute.internal>
2015-09-07 12:59:20 +0900

    validates on

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -2,6 +2,6 @@ class Borrowing < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
 
-#  validates :user_id, presence: true
+  validates :user_id, presence: true
   validates :book_id, presence: true
 end
