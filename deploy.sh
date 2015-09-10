54f5b9e11eac619110d6803884f0780743801600
1stbit-ito <rails@ip-172-31-18-163.ap-northeast-1.compute.internal>
2015-09-10 17:24:53 +0900

    deploy error handling add

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
 ssh-add -l
 
-bundle exec cap staging deploy
+### bundle exec cap staging deploy
 ### bundle exec cap development deploy:check
-### bundle exec cap development deploy
+bundle exec cap development deploy || RET=$?
 
 ssh-agent -k
+
+exit $RET
