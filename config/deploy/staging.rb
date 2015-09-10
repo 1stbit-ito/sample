3103f156dd1024c783fe2dcc2243dce9f59d6e74
1stbit-ito <rails@ip-172-31-18-163.ap-northeast-1.compute.internal>
2015-09-10 12:55:25 +0900

    add staging-web03

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,9 +5,9 @@ set :branch, 'master'
 set :rails_env, 'staging'
 set :migration_role, 'db'
  
-role :app, %w{rails@172.31.24.164 rails@172.31.24.165} # 実際のサーバのIPアドレスを指定します。
-role :web, %w{rails@172.31.24.164 rails@172.31.24.165}
-role :db, %w{rails@172.31.24.164 rails@172.31.24.165}
+role :app, %w{rails@172.31.24.164 rails@172.31.24.165 rails@172.31.6.15} # 実際のサーバのIPアドレスを指定します。
+role :web, %w{rails@172.31.24.164 rails@172.31.24.165 rails@172.31.6.15}
+role :db, %w{rails@172.31.24.164 rails@172.31.24.165 rails@172.31.6.15}
  
 set :ssh_options, {
 keys: [File.expand_path('~/.ssh/id_rsa')],
@@ -17,3 +17,4 @@ auth_methods: %w(publickey)
  
 server '172.31.24.164', user: 'rails', roles: %w{web app db}
 server '172.31.24.165', user: 'rails', roles: %w{web app db}
+server '172.31.6.15', user: 'rails', roles: %w{web app db}
